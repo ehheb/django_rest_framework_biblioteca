f00064d1add39566572b2c633343563e15c2540f
hebert <hebertvillarreal1@gmail.com>
2021-04-10 11:21:42 -0600

    Se realizan algunos ajustes en el código, proyecto completado

--- autores/serializers.py
+++ autores/serializers.py
@@ -1,10 +1,7 @@
 from rest_framework.serializers import ModelSerializer
 from autores.models import Autor
-#from libros.serializers import LibroSerializer
 
 class AutorSerializer(ModelSerializer):
-
-    #libros = LibroSerializer(many=True, read_only=True)
 
     class Meta:
         model = Autor
--- libros/serializers.py
+++ libros/serializers.py
@@ -1,10 +1,7 @@
 from rest_framework.serializers import ModelSerializer
 from libros.models import Libro
-from autores.serializers import AutorSerializer
 
 class LibroSerializer(ModelSerializer):
-
-    autores = AutorSerializer(many=True, read_only=True)
 
     class Meta:
         model = Libro
